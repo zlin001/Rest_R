9c82a57b90748be8697a53071c086a6eb6481120
LiuYuHao <liuyuhao717@gmail.com>
2018-02-13 23:12:33 -0500

    use rake to do key extraction and save the data into a file and organize it

---
+++ key_extraction_rake.py
@@ -0,0 +1,12 @@
+from rake_nltk import Rake
+stoppath = "SmartStoplist.txt"
+rake = Rake()
+with open('reviews.txt', 'r') as f_reviews, open('keywords.txt', 'w') as f_keywords:
+    reviews = f_reviews.read()
+    rake.extract_keywords_from_text(reviews)
+    result = rake.get_ranked_phrases_with_scores()
+    for i in range(len(result)):
+        for b in range(len(result[i])):
+            f_keywords.write(str(result[i][b]))
+            f_keywords.write(" ")
+        f_keywords.write("\n")
