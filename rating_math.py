fd9db3983c005bc48446fc78e81c442075030a91
LiuYuHao <liuyuhao717@gmail.com>
2018-03-20 12:17:59 -0400

    complete the math rating with threshold need check my code

--- rating_math.py
+++ rating_math.py
@@ -1,34 +1,43 @@
-def separate_aspect(sent,aspects,count_aspects):
-    if sent['aspect'] in aspects:
-        count_aspects[sent['aspect']] += 1.0
+import numpy
+def separate_aspect(sent,aspects_all,count_aspects_all):
+    if sent['aspect'] in aspects_all:
+        count_aspects_all[sent['aspect']] += 1.0
     else:
-        count_aspects[sent['aspect']] = 1.0
-        aspects[sent['aspect']] = 0.0
-#def calculation(aspects,all_sent,count_aspects):
-#    for sent in all_sent:
-#        dic_sent = eval(sent)
-#        if dic_sent['polarity'] == 'positive':
-#            aspects[dic_sent['aspect']] += float(dic_sent['polarity_confidence']) * float(dic_sent['aspect_confidence'])
-#        elif dic_sent['polarity'] == 'negative':
-#            aspects[dic_sent['aspect']] += (1.0 - float(dic_sent['polarity_confidence'])) * float(dic_sent['aspect_confidence'])
-#    for aspect in aspects:
-#        aspects[aspect] = aspects[aspect] / count_aspects[aspect]
-def calculation_v2(aspects,all_sent,count_aspects):
+        count_aspects_all[sent['aspect']] = 1.0
+        aspects_all[sent['aspect']] = 0.0
+def separate_aspect_with_threshold(sent,aspects_threshold,aspects_filter,count_aspects_filter):
+    if sent['aspect_confidence'] >= aspects_threshold[sent['aspect']]:
+        if sent['aspect'] in aspects_filter:
+            count_aspects_filter[sent['aspect']] += 1.0
+        else:
+            count_aspects_filter[sent['aspect']] = 1.0
+            aspects_filter[sent['aspect']] = 0.0
+def calculation_filter_with_threshold(aspects_threshold,aspects_filter,all_sent,count_aspects_filter):
     for sent in all_sent:
         dic_sent = eval(sent)
-        if dic_sent['polarity'] == 'positive':
-            aspects[dic_sent['aspect']] += float(dic_sent['polarity_confidence'])
-        elif dic_sent['polarity'] == 'negative':
-            aspects[dic_sent['aspect']] += (1.0 - float(dic_sent['polarity_confidence']))
+        if dic_sent['aspect_confidence'] >= aspects_threshold[dic_sent['aspect']]:
+            if dic_sent['polarity'] == 'positive':
+                aspects_filter[dic_sent['aspect']] += float(dic_sent['polarity_confidence'])
+            elif dic_sent['polarity'] == 'negative':
+                aspects_filter[dic_sent['aspect']] += (1.0 - float(dic_sent['polarity_confidence']))
+    for aspect in aspects_filter:
+        aspects_filter[aspect] = aspects_filter[aspect] / count_aspects_filter[aspect]
+def calculation_threshold_aspect(aspects,all_sent,count_aspects):
+    for sent in all_sent:
+        dic_sent = eval(sent)
+        aspects[dic_sent['aspect']] += float(dic_sent['aspect_confidence'])
     for aspect in aspects:
         aspects[aspect] = aspects[aspect] / count_aspects[aspect]
 with open('sentiment.txt', 'rb') as f_sent:
-    aspects = {}
-    count_aspects = {}
+    aspects_threshold = {}
+    count_aspects_all = {}
+    aspects_filter = {}
+    count_aspects_filter = {}
     all_sent = f_sent.read().splitlines()
     for sent in all_sent:
-        separate_aspect(eval(sent),aspects,count_aspects)
-        test = eval(sent)
-    calculation_v2(aspects,all_sent,count_aspects)
-print(aspects)
-print(count_aspects)
+        separate_aspect(eval(sent),aspects_threshold,count_aspects_all)
+    calculation_threshold_aspect(aspects_threshold,all_sent,count_aspects_all)
+    for sent in all_sent:
+        separate_aspect_with_threshold(eval(sent),aspects_threshold,aspects_filter,count_aspects_filter)
+    calculation_filter_with_threshold(aspects_threshold,aspects_filter,all_sent,count_aspects_filter)
+print(aspects_filter)
