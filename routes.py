b7a56de0aecbd41f35c2923aa8b770d1e3e562cb
Arvin Jin <ArvinJin@Arvins-Air.fios-router.home>
2018-04-27 18:35:47 -0400

    modified routes.py and added scrape rank page and restaurant links

--- routes.py
+++ routes.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/enter_zip_code', methods=["GET",'POST'])
+@app.route("/enter_zip_code", methods=["POST"])
 def enter_zip_code():
     zip_code = request.form.get("zip_code")
-    return render_template("top_ten_rests.html", top10=None)
-
+    return render_template("top_ten_rests.html", zip_code=zip_code)
 
 
 if __name__ == '__main__':
