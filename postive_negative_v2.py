671514ad7be7d79f7ac02177ed3f24acd0ce35be
LiuYuHao <liuyuhao717@gmail.com>
2018-05-12 16:56:30 -0400

    minor fix

--- postive_negative_v2.py
+++ postive_negative_v2.py
@@ -110,10 +110,10 @@ def get_all_scores(restuarants):
     for restuarant in restuarants:
         data.append(restuarant[1])
     with Pool(31) as p:
-        print(p.map(all_scores, data))
+        all_scores = p.map(all_scores, data)
         p.terminate()
         p.join()
-
+    return all_scores
 #print(all_score)
 # with open("reviews_score.txt",'w',encoding='windows-1252') as f_reviews_score, open("final_score.txt",'w',encoding="windows-1252") as f_final_score:
 #     for i in range(31):
